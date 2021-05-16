6bb4364d79562eeeb121f90c225fb6cad98fa5e5
jac <jacleighp@gmail.com>
2021-05-16 22:51:04 +0100

    update format + add dropdown + new contact form

--- js/colors.js
+++ js/colors.js
@@ -1,23 +1,23 @@
 // get the system theme media query and localStorage theme
-let systemIsDark = window.matchMedia('(prefers-color-scheme: dark)');
-let theme = localStorage.getItem('color-mode-toggle');
+let systemIsDark = window.matchMedia("(prefers-color-scheme: dark)");
+let theme = localStorage.getItem("color-mode-toggle");
 
 // set color model to dark mode
 function darkMode() {
-  document.documentElement.setAttribute('theme', 'dark');
-  localStorage.setItem('color-mode-toggle', 'dark');
+  document.documentElement.setAttribute("theme", "dark");
+  localStorage.setItem("color-mode-toggle", "dark");
 }
 
 // set color mode to light mode
 function lightMode() {
-  document.documentElement.setAttribute('theme', 'light');
-  localStorage.setItem('color-mode-toggle', 'light');
+  document.documentElement.setAttribute("theme", "light");
+  localStorage.setItem("color-mode-toggle", "light");
 }
 
 // default color mode on reload
-if (theme === 'dark') {
+if (theme === "dark") {
   darkMode();
-} else if (theme === 'light') {
+} else if (theme === "light") {
   lightMode();
 } else if (systemIsDark.matches) {
   darkMode();
@@ -27,11 +27,11 @@ if (theme === 'dark') {
 
 // toggle the color mode
 function toggleColorMode() {
-  let theme = localStorage.getItem('color-mode-toggle');
+  let theme = localStorage.getItem("color-mode-toggle");
 
-  if (theme === 'dark') {
+  if (theme === "dark") {
     lightMode();
-  } else if (theme === 'light') {
+  } else if (theme === "light") {
     darkMode();
   } else if (systemIsDark.matches) {
     lightMode();
@@ -40,7 +40,7 @@ function toggleColorMode() {
   }
 }
 
-toggles = document.getElementsByClassName('color-mode-toggle');
+toggles = document.getElementsByClassName("color-mode-toggle");
 console.log(toggles);
 for (var i = 0; i < toggles.length; i++) {
   toggles[i].onclick = toggleColorMode;
